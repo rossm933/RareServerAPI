2ab5045c50fcc722ce63f202f6c356816f65673e
Ross Morgan <ross.morgan933@gmail.com>
2024-08-06 18:09:06 -0500

    API calls for get, delete, create, details, and get by userid

--- Models/Posts.cs
+++ Models/Posts.cs
@@ -2,5 +2,16 @@
 {
     public class Posts
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public string? Title { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public string? Content { get; set; }
+
+        public bool Approved { get; set; }
+
     }
 }
